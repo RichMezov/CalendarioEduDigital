6b5ccd37ff6cac5beafaa009eb6337679971f232
Márcio <95216996+Marcio-py@users.noreply.github.com>
2023-04-23 12:43:30 +0100

    Complete function DisplayOcurrences
    Now every time we choose a day in the calendar, we se automatically its ocurrences on events section

--- Calendar/app.js
+++ Calendar/app.js
@@ -1,23 +1,15 @@
-const date = new Date();
-
-const renderCalendar = () => {
+// pegar as data(dia/mes/ano)
+const event_btn = document.querySelector(".events_btn")
 const monthDays = document.querySelector(".days");
+const current_month = document.querySelector(".date h1");
+const current_fulldate = document.querySelector('.date p')
+const daySelector = document.querySelector(".days")
+const newEventButton = document.querySelector(".add")
+var event_date;
 
 
-const lastDay = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate();
-
-
-const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
-
-const firstDayIndex = date.getDay();
-
-const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-).getDay();
-
-const nextDays = 7 - lastDayIndex - 1;
+const date = new Date();
+const current_year = date.getFullYear()
 
 const months = [
     "Janeiro",
@@ -32,82 +24,152 @@ const months = [
     "Outubro",
     "Novembro",
     "Dezembro",
-
+    
 ];
 
-document.querySelector(".date h1").innerHTML
-= months[date.getMonth()];
 
-document.querySelector('.date p').innerHTML
-=new Date().toDateString();
-
-let days = "";
-
-for(let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
-}
-/* _____________________*/
-
-/*_____________________________________ */
-for (let i = 1; i <= lastDay; i++) {
-    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
-        days += `<div class="today">${i}</div>`;
+class Store {
+    static getEvents() {
+        let arr_events; // Onde os objectos eventos sao armazenados
+      //verificar se há um objeto event dentro do local storage, se não tiver trás 1 array vazia
+      if (localStorage.getItem("events") === null ) {
+        arr_events = [];
+      } else {
+        arr_events = JSON.parse(localStorage.getItem('arr_events'));
+      }
+  
+      return arr_events;
     }
-    else
-    days += `<div>${i}</div>`;
-}
+  
+    static addEvents(event) {
+      // pegar eventos armazenados
+      const arr_events = Store.getEvents();
+  
+      // Iterar sobre a array events com o forEach, com os parametros event e index
+      arr_events.forEach((event, index) => {
+  
+        if( event.description === description) {
+          // dividir o iterado atual da array pelo seu index
+          arr_events.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('arr_events', JSON.stringify(arr_events)); // Atualizar items no local storage apos a adiçao
+    }
+  
+    static removeEvent(description) {
+      // conseguir os livros armazenados
+      const arr_events = Store.getEvents();
+  
+      // iterar sobre a array de livros armazenados
+      arr_events.forEach((event, index) => {
+        if(event.description === description) {
+        // a funçao splice pode ser usada para subistiruir, ou apagar certo item splice os metodos são (start, deleteCount, item1)  
+          arr_events.splice(index,1)
+        }
+      });
+      //local storage, recebe um item, com a key events, valor events(versao string)
+      localStorage.setItem("arr_events", JSON.stringify(arr_events)) // Atualizar items no local storage apos a adiçao
+    }
+  }
 
-for(let j = 1; j<=nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
+const arr_events = Store.getEvents();//acessar eventos na classe store
+// Inicio  da funçao que adiciona os dias do mess
+const renderCalendar = () => {
+
+    //pegar dias do mes
+    let firstDayIndex = new Date(current_year, date.getMonth(), 1).getDay();
+    lastDayMonth = new Date(current_year, date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(current_year, date.getMonth(), 0).getDate();
+    const lastDayIndex = new Date(current_year, date.getMonth() + 1, 0).getDay();
+    const nextDays = 7 - lastDayIndex - 1;
+    current_month.innerHTML = months[date.getMonth()];
+    current_fulldate.innerHTML = new Date().toDateString();
+
+    let days = "";
+
+    // retorna Dias do mes anterior
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">
+            ${prevLastDay - x}
+            </div>`
+    }
+    //iteraçao que retorna Dias do mes atual
+    for (let i = 1; i <= lastDayMonth; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today">${i}</div>`;
+        }
+        else
+            days += `<div>${i}</div>`;
+    }
+
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+    }
     monthDays.innerHTML = days;
 }
-}
 
-document.querySelector(".prev").addEventListener("click", () =>{
+// inicio de slider entre os meses um slider entre os meses
+document.querySelector(".prev").addEventListener("click", () => {
     date.setMonth(date.getMonth() - 1);
     renderCalendar();
 })
 
-document.querySelector(".next").addEventListener("click", () =>{
+document.querySelector(".next").addEventListener("click", () => {
     date.setMonth(date.getMonth() + 1);
     renderCalendar();
 })
-
-
-
+// Fim de slider entre os meses um slider entre os meses
 renderCalendar();
 
-//New changes
-const info = document.querySelector(".info");
-const infoContainer = document.querySelector(".infoDisplay");
-let counter = 0;
+event_btn.addEventListener("click", function () {
+    const event_info = document.getElementById("info")
+    event_info.classList.toggle("hide") // no click, se tiver event_info tiver a classe hide, essa classe sera removida... se event_info nao tiver a classe hide ela será adicionada
+})
 
-const diaTest = document.querySelector(".divTest")
-let events = "";
-let isOpen = false;
+class Ocurrence {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
 
-function openInfo() {
-    if(counter <= 5) {
+function saveOcurrence() {
+
+    let dateInfo = document.querySelector(".dateInfo");
+
+    daySelector.addEventListener("click", (e) => {
+        const el = e.target.innerHTML;
+        event_date = `${el}-${current_month.innerHTML}-${current_year}`;
+        dateInfo.innerText = event_date;
+        displayOcurrences(event_date) 
+    })
+
+    newEventButton.addEventListener("click", function () {
+        let descInfo = document.querySelector(".descInfo").value
+        const ocurrence = new Ocurrence(event_date, descInfo.split()) 
         
-        events += `<div class="event"><h3>tile of event</h3>
-        <p>descricao do evento pode ser grande</p></div>`
-        infoContainer.innerHTML = events
-        counter++;
-    };
+        if ( event_date == undefined || descInfo == '') {
+            alert("Por favor, Seleciona uma data e descriçao para a ocorrencia")
+        } else {
+            Store.addEvents(ocurrence);
+            descInfo = document.querySelector(".descInfo").value = ""
+        }
+    })
 }
+saveOcurrence()
 
-function openEvents() {
-    
+ function displayOcurrences(ocurrenceDate) {
+    const filterdates = arr_events.filter(events => events.date == ocurrenceDate)// Receber apenas a descriçao das datas desejadas
+    const ocurrence_container = document.querySelector(".ocurrence_container");
+    // iterar a const filterdates forEach, vamos trazer uma tmplate string com date e descreiption
+    const renderOcurrences = filterdates.map(ocurrenceElement => {
+        return `<tr>
+                    <th scope="row">${ocurrenceElement.date}</th>
+                    <td>${ocurrenceElement.description}</td>
+                </tr>`;
+    });
+    ocurrence_container.innerHTML = renderOcurrences;
 
-    if(isOpen === false) {
-        info.style.display = "block";
-        isOpen = true;
-        console.log(isOpen)
-    }
-    else if(isOpen === true) {
-        info.style.display = "none";
-        isOpen = false;
-    }
-}
-
-
+    console.log(filterdates)
+ }
