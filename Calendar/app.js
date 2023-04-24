5c2a48c31d10549fc62a10e36ce3d1bf4111388a
Márcio <95216996+Marcio-py@users.noreply.github.com>
2023-04-24 15:04:35 +0100

    Add more classes
    Now the code is easier to debug, more scalable and we can delete the ocurrences in case of errors

--- Calendar/app.js
+++ Calendar/app.js
@@ -1,4 +1,3 @@
-// pegar as data(dia/mes/ano)
 const event_btn = document.querySelector(".events_btn")
 const monthDays = document.querySelector(".days");
 const current_month = document.querySelector(".date h1");
@@ -10,152 +9,151 @@ var event_date;
 const date = new Date();
 const current_year = date.getFullYear()
 
-const ocurrenceList = [ ]
+const ocurrenceList = []
 
 const months = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-    
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+
 ];
 
 const renderCalendar = () => {
 
-    //pegar dias do mes
-    let firstDayIndex = new Date(current_year, date.getMonth(), 1).getDay();
-    lastDayMonth = new Date(current_year, date.getMonth() + 1, 0).getDate();
-    const prevLastDay = new Date(current_year, date.getMonth(), 0).getDate();
-    const lastDayIndex = new Date(current_year, date.getMonth() + 1, 0).getDay();
-    const nextDays = 7 - lastDayIndex - 1;
-    current_month.innerHTML = months[date.getMonth()];
-    current_fulldate.innerHTML = new Date().toDateString();
+  //pegar dias do mes
+  let firstDayIndex = new Date(current_year, date.getMonth(), 1).getDay();
+  lastDayMonth = new Date(current_year, date.getMonth() + 1, 0).getDate();
+  const prevLastDay = new Date(current_year, date.getMonth(), 0).getDate();
+  const lastDayIndex = new Date(current_year, date.getMonth() + 1, 0).getDay();
+  const nextDays = 7 - lastDayIndex - 1;
+  current_month.innerHTML = months[date.getMonth()];
+  current_fulldate.innerHTML = new Date().toDateString();
 
-    let days = "";
+  let days = "";
 
-    // retorna Dias do mes anterior
-    for (let x = firstDayIndex; x > 0; x--) {
-        days += `<div class="prev-date">
+  // retorna Dias do mes anterior
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">
             ${prevLastDay - x}
             </div>`
+  }
+  //iteraçao que retorna Dias do mes atual
+  for (let i = 1; i <= lastDayMonth; i++) {
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      days += `<div class="today">${i}</div>`;
     }
-    //iteraçao que retorna Dias do mes atual
-    for (let i = 1; i <= lastDayMonth; i++) {
-        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-            days += `<div class="today">${i}</div>`;
-        }
-        else
-            days += `<div>${i}</div>`;
-    }
+    else
+      days += `<div>${i}</div>`;
+  }
 
-    for (let j = 1; j <= nextDays; j++) {
-        days += `<div class="next-date">${j}</div>`;
-    }
-    monthDays.innerHTML = days;
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+  }
+  monthDays.innerHTML = days;
 }
 // inicio de slider entre os meses um slider entre os meses
 document.querySelector(".prev").addEventListener("click", () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalendar();
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
 })
 
 document.querySelector(".next").addEventListener("click", () => {
-    date.setMonth(date.getMonth() + 1);
-    renderCalendar();
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
 })
 // Fim de slider entre os meses um slider entre os meses
 renderCalendar();
 
 event_btn.addEventListener("click", function () {
-    const event_info = document.getElementById("info")
-    event_info.classList.toggle("hide") // no click, se tiver event_info tiver a classe hide, essa classe sera removida... se event_info nao tiver a classe hide ela será adicionada
+  const event_info = document.getElementById("info")
+  event_info.classList.toggle("hide") // no click, se tiver event_info tiver a classe hide, essa classe sera removida... se event_info nao tiver a classe hide ela será adicionada
 })
 
 
 class Event {
-    constructor(date, description) {
-        this.date = date;
-        this.description = description;
-    }
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
 }
 
 class UI {
-    static displayEvents() { // verificado
-      const events = Store.getEvents();
-  
-      events.forEach((event) => UI.addOcurrenceToList(event));
-    }
-  
-    static addOcurrenceToList(event) {
+  static displayEvents(eventDate) { // verificado
 
-      const list = document.querySelector('#event-list');
+    const events = Store.getEvents();
+    const filterdates = events.filter(event => event.date == eventDate);
+    const list = document.querySelector('#event-list');
+    
 
-      const row = document.createElement('tr');
-  
-      row.innerHTML = `
-        <td>${event.title}</td>
+    // o innerhtml da list, recebe iteraçao da filtered array que retorna o valor abaixo
+    const currentEvents = filterdates.map(event => {
+      return `</tr>
+        <td>${event.date}</td>
         <td>${event.description}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        </tr>
       `;
-      // adicionar novo evento a table
-      list.appendChild(row);
-    }
+  });
 
-    // Deletes a targeted element(event)
-    static deleteEvent(el) {
-      // Check if element has the class "delete"
-      if(el.classList.contains('delete')) {
-        // Remove "parent el" of the "parent el" (2 levels up to get entire row)
-        el.parentElement.parentElement.remove();
-      }
-    }
-  
-    // Alert
-    static showAlert(message, className) {
+  list.innerHTML = currentEvents;  
+  }
 
-      const div = document.createElement('div');
-
-      div.className = `alert alert-${className}`;
-
-      div.appendChild(document.createTextNode(message));
-
-      const container = document.querySelector('.container'); // ainda não temos um container
-
-      const form = document.querySelector('#event-form'); // ainda nao temos um event form
-
-      container.insertBefore(div, form);
-
-      setTimeout(() => document.querySelector('.alert').remove(), 3000);
-    }
-  
-    // Clears fields after submission
-    static clearFields() {
-      document.querySelector('.descInfo').value = ''; // vamos mudar para agir sobre sobre um input que vai conter texto da ocorrencia
+  // Deletes a targeted element(event)
+  static deleteEvent(el) {
+    // Check if element has the class "delete"
+    if (el.classList.contains('delete')) {
+      // Remove "parent el" of the "parent el" (2 levels up to get entire row)
+      el.parentElement.parentElement.remove();
     }
   }
 
+  // Alert
+  static showAlert(message, className) {
+
+    const div = document.createElement('div');
+
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('.container'); // ainda não temos um container
+
+    const form = document.querySelector('#event-form'); // ainda nao temos um event form
+
+    container.insertBefore(div, form);
+
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+  // Clears fields after submission
+  static clearFields() {
+    document.querySelector('.descInfo').value = ''; // vamos mudar para agir sobre sobre um input que vai conter texto da ocorrencia
+  }
+}
+
 class Store { // esta classe ja foi revisada por completo
-    static getEvents() {
+  static getEvents() {
     let events
 
-    if(localStorage.getItem("events") === null) {
-        events = []
+    if (localStorage.getItem("events") === null) {
+      events = []
     } else {
-        events = JSON.parse(localStorage.getItem("events"))
+      events = JSON.parse(localStorage.getItem("events"))
     }
 
     return events
-   }
+  }
 
-    static addEvent(event) {
+  static addEvent(event) {
 
     const events = Store.getEvents();
 
@@ -165,15 +163,13 @@ class Store { // esta classe ja foi revisada por completo
   }
 
 
-    static removeEvent(description) {
+  static removeEvent(description) {
 
     const events = Store.getEvents();
 
 
     events.forEach((event, index) => {
-
-      if(event.description === description) {
-
+      if (event.description === description) {
         events.splice(index, 1);
       }
     });
@@ -184,37 +180,36 @@ class Store { // esta classe ja foi revisada por completo
 
 function saveOcurrence() {
 
-    let dateInfo = document.querySelector(".dateInfo");
+  let dateInfo = document.querySelector(".dateInfo");
 
-    daySelector.addEventListener("click", (e) => {
-        const el = e.target.innerHTML;
-        event_date = `${el}-${current_month.innerHTML}-${current_year}`;
-        dateInfo.innerText = event_date;
-        displayOcurrences(event_date) 
-    })
+  daySelector.addEventListener("click", (e) => {
+    const el = e.target.innerHTML;
+    event_date = `${el}-${current_month.innerHTML}-${current_year}`;
+    dateInfo.innerText = event_date;
+    UI.displayEvents(event_date)
+  })
 
-    newEventButton.addEventListener("click", function () {
-        let descInfo = document.querySelector(".descInfo").value
-        const event = new Event(event_date, descInfo.split())
-        
-        if ( event_date == undefined || descInfo == '') {
-            alert("Por favor, Seleciona uma data e descriçao para a ocorrencia")
-        } else {
-            Store.addEvent(event) // adicionar a arrat de eventos na store
-            UI.clearFields();
-        }
-    })
+  newEventButton.addEventListener("click", function () {
+    let descInfo = document.querySelector(".descInfo").value
+    const event = new Event(dateInfo.innerHTML, descInfo)
+
+    if (event_date == undefined || descInfo == '') {
+      alert("Por favor, Seleciona uma data e descriçao para a ocorrencia")
+    } else {
+      Store.addEvent(event) // adicionar a arrat de eventos na store
+      UI.clearFields();
+    }
+  })
 }
+
+document.querySelector('#event-list').addEventListener('click', (e) => {
+
+
+  UI.displayEvents(e.target);
+
+ Store.removeEvent(e.target.parentElement.previousElementSibling.textContent);
+
+  // UI.showAlert('Book Removed', 'success');
+});
+
 saveOcurrence()
-
- function displayOcurrences(ocurrenceDate) {
-
-    const events = Store.getEvents();
-
-    const filterdates = events.filter(event => event.date == ocurrenceDate)// Receber apenas a descriçao das datas desejadas
-    // iterar a const filterdates forEach, vamos trazer uma tmplate string com date e descreiption
-    const renderOcurrences = filterdates.map(event => UI.addOcurrenceToList(event));
-    ocurrence_container.innerHTML = renderOcurrences;
-
-    console.log(filterdates)
- }
